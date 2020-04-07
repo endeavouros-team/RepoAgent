6979e0f0c9d032dba159a648daef6cb5dfce75e0
AlpixTM <contact@alpix.eu>
2020-04-07 19:01:14 +0200

    Add Mirror Model

--- agent/admin.py
+++ agent/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mirror
+
+admin.site.register(Mirror)
--- agent/models.py
+++ agent/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Mirror(models.Model):
+    name = models.CharField(max_length=30)
+    url = models.CharField(max_length=34)
+    sponsor_url = models.CharField(max_length=64, blank=True)
+
